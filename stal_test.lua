b0d544b13583faf2477b0779e9cd158fbaabe75e
Michel Martens <michel@soveran.com>
2016-12-23 10:26:16 +0100

    Avoid replication only when the feature is available

--- stal_test.lua
+++ stal_test.lua
@@ -21,7 +21,7 @@ assert_equal("2\n3\n", solve('["SINTER", "A", "B"]'))
 assert_equal("1\n5\n", solve('["SDIFF", ["SUNION", "A", "C"], "B"]'))
 
 -- Verify there's no keyspace pollution
-assert_equal("B\nC\nA\n", solve('["KEYS", "*"]'))
+assert_equal("3\n", solve('["DBSIZE"]'))
 
 -- Operations with sorted sets
 redis("FLUSHDB")
@@ -32,7 +32,7 @@ redis("ZADD C 1 d 2 e 3 f")
 assert_equal("b\nc\n", solve('["ZRANGE", ["ZINTER", "2", "A", "B"], "0", "-1"]'))
 
 -- Verify there's no keyspace pollution
-assert_equal("B\nC\nA\n", solve('["KEYS", "*"]'))
+assert_equal("3\n", solve('["DBSIZE"]'))
 
 -- Operations with sorted and unsorted sets
 redis("FLUSHDB")
@@ -42,4 +42,4 @@ redis("SADD B b c d")
 assert_equal("b\nc\n", solve('["ZRANGE", ["ZINTER", "2", "A", "B"], "0", "-1"]'))
 
 -- Verify there's no keyspace pollution
-assert_equal("B\nA\n", solve('["KEYS", "*"]'))
+assert_equal("2\n", solve('["DBSIZE"]'))
